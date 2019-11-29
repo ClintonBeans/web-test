854372abfae4cc18323384abcf5c52f5c4f792b7
lixiaopengbean <lixiaopengbean@163.com>
2019-11-29 11:12:49 +0800

    init

---
+++ src/main/java/com/abc/controller/SomeController.java
@@ -0,0 +1,18 @@
+package com.abc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: lixiaopeng
+ * @date: 2019/11/29 10:21
+ * @desc:
+ */
+@RestController
+public class SomeController {
+
+    @RequestMapping("/some")
+    public String someHandle() {
+        return "This is first jenkins deploy project";
+    }
+}
